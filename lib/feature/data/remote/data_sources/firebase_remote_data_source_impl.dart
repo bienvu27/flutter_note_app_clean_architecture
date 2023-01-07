c1b4b2d1d2591a2b09e28d9a9e95e455401bb9bd
BienVu <bienvx1999@gmail.com>
2023-01-07 23:26:05 +0700

    update

--- lib/feature/data/remote/data_sources/firebase_remote_data_source_impl.dart
+++ lib/feature/data/remote/data_sources/firebase_remote_data_source_impl.dart
@@ -1,65 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:note_app_clean_architecture/feature/data/remote/data_sources/firebase_remote_data_source.dart';
+import 'package:note_app_clean_architecture/feature/data/remote/models/note_model.dart';
+import 'package:note_app_clean_architecture/feature/data/remote/models/user_model.dart';
 import 'package:note_app_clean_architecture/feature/domain/entities/note_entity.dart';
 import 'package:note_app_clean_architecture/feature/domain/entities/user_entity.dart';
 
 class FirebaseRemoteDataSourceImpl implements FirebaseRemoteDataSource {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+
+  FirebaseRemoteDataSourceImpl({required this.auth, required this.firestore});
+
   @override
-  Future<void> addNewNote(NoteEntity note) {
-    // TODO: implement addNewNote
-    throw UnimplementedError();
+  Future<void> addNewNote(NoteEntity noteEntity) async {
+    final noteCollectionRef = firestore.collection("users").doc(noteEntity.uid).collection("notes");
+    final noteId = noteCollectionRef.doc().id;
+    noteCollectionRef.doc(noteId).get().then((note) {
+      final newNote =
+          NoteModel(uid: noteEntity.uid, noteId: noteId, note: noteEntity.note, time: noteEntity.time).toDocument();
+      if (!note.exists) {
+        noteCollectionRef.doc(noteId).set(newNote);
+      }
+      return;
+    });
   }
 
   @override
-  Future<void> deleteNote(NoteEntity note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(NoteEntity noteEntity) async {
+    final noteCollectionRef = firestore.collection("users").doc(noteEntity.uid).collection("notes");
+    noteCollectionRef.doc(noteEntity.noteId).get().then((note) {
+      if (note.exists) {
+        noteCollectionRef.doc(noteEntity.noteId).delete();
+      }
+      return;
+    });
   }
 
   @override
-  Future<void> getCreateCurrentUser(UserEntity user) {
-    // TODO: implement getCreateCurrentUser
-    throw UnimplementedError();
+  Future<void> getCreateCurrentUser(UserEntity user) async {
+    final userConllectionRef = firestore.collection("users");
+    final uid = await getCurrentUId();
+    userConllectionRef.doc(user.uid).get().then((value) {
+      final newUser = UserModel(
+        uid: uid,
+        status: user.status,
+        email: user.email,
+        name: user.name,
+      ).toDocument();
+      if (!value.exists) {
+        userConllectionRef.doc(uid).set(newUser);
+      }
+      return;
+    });
   }
 
   @override
-  Future<String> getCurrentUId() {
-    // TODO: implement getCurrentUId
-    throw UnimplementedError();
-  }
+  Future<String> getCurrentUId() async => auth.currentUser!.uid;
 
   @override
   Stream<List<NoteEntity>> getNotes(String uid) {
-    // TODO: implement getNotes
-    throw UnimplementedError();
+    final noteConllectionRef = firestore.collection("users").doc(uid).collection("notes");
+    return noteConllectionRef.snapshots().map((querySnap) {
+      return querySnap.docs.map((docSnap) => NoteModel.fromSnapshot(docSnap)).toList();
+    });
   }
 
   @override
-  Future<bool> isSignIn() {
-    // TODO: implement isSignIn
-    throw UnimplementedError();
-  }
+  Future<bool> isSignIn() async => auth.currentUser?.uid != null;
 
   @override
-  Future<void> signIn(UserEntity user) {
-    // TODO: implement signIn
-    throw UnimplementedError();
-  }
+  Future<void> signIn(UserEntity user) => auth.signInWithEmailAndPassword(email: user.email!, password: user.password!);
 
   @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
-  }
+  Future<void> signOut() async => auth.signOut();
 
   @override
-  Future<void> signUp(UserEntity user) {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
+  Future<void> signUp(UserEntity user) async =>
+      auth.createUserWithEmailAndPassword(email: user.email!, password: user.password!);
 
   @override
-  Future<void> updateNote(NoteEntity note) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<void> updateNote(NoteEntity note) async {
+    Map<String, dynamic> noteMap = Map();
+    final noteConllectionRef = firestore.collection("users").doc(note.uid).collection("notes");
+    if (note.note != null) noteMap['note'] = note.note;
+    if (note.time != null) noteMap['time'] = note.time;
+
+    noteConllectionRef.doc(note.noteId).update(noteMap);
   }
 }
